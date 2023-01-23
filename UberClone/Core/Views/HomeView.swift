a5d5bb74abbb23e01b0b1badd3069018d1629b5d
Mominul <mominul.islam@digigate30.com>
2023-01-24 02:42:07 +0600

    map init

---
+++ UberClone/Core/Views/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  UberClone
+//
+//  Created by Mac Mini 5 on 24/1/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        UberMapViewRepresentable()
+            .ignoresSafeArea()
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
